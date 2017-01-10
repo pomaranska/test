1cfb1dcd2bc535ffe61bbc460756c7c7bb32fd64
Aleksandra Pomarańska <Aleksandra Pomaraska>
2017-01-10 21:19:11 +0100

    testprog

---
+++ test/src/test/Test.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Ola
+ */
+public class Test {
+
+        private int testowy;
+        private int tescik;
+        private String slowo;
+        
+        public Test(int testowy, int tescik, String slowo){
+            
+        }
+        
+        public void test(int i){
+
+        }
+}
